c69f2cb3fe64639615c98a9843926903781a4cb9
Romas Yurii <romas_yura@ukr.net>
2026-01-16 20:40:42 +0200

    Comment out database drop in init_db

--- relational_db.py
+++ relational_db.py
@@ -30,7 +30,7 @@ async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+        #await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 async def add_user(name: str):
